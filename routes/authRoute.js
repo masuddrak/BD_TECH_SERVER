55d2eda66445e405d3b071bb72628408e54f3301
masud <masudranaacb@gmail.com>
2025-03-14 05:17:59 +0600

    2 step verification create with nodemailer

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,6 +6,7 @@ router.post('/signup', authController.signUp)
 router.post('/signin', authController.signin)
 router.post('/signout', authController.signout)
 router.patch('/send-verification', authController.sendVerificationCode)
+router.patch('/verification-code', authController.verifyVerificationCode)
 
 module.exports = router
 
