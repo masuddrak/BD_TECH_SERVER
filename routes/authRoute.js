6dd4bfe510821c51d4cb904f496f2fc9ca245169
masud <masudranaacb@gmail.com>
2025-03-13 23:09:54 +0600

    sigin function create with jwt token

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const authController=require("../controllers/authController")
+const authController = require("../controllers/authController")
+// route
 router.post('/signup', authController.signUp)
+router.post('/signin', authController.signin)
 
 module.exports = router
 
